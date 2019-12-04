1f9517a10e78cf43dcde9195d2114cf7d9f92549
zry-web <1607153998@qq.com>
2019-12-04 09:30:43 +0800

    创建了完整目录，以及login页面

--- README.md
+++ README.md
@@ -18,3 +18,25 @@ npm run build
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
 aaaaa
+
+###目录结构
+```bash
+api		存放各类需要与后端交互获取数据的api
+
+assets		存放项目中需要用到的资源文件，css、js、images等
+
+router		vue-router vue路由的配置文件
+
+components	存放vue开发中一些公共组件：header.vue、footer.vue等
+
+store		vuex进行数据流控制的仓库
+        moudles 	拆分目录
+
+utils		存放vue开发过程中一些公共的.js方法
+
+views		存放vue页面组件的文件夹
+
+main.js		vue-cli工程的入口文件
+
+app.vue		使用标签<route-view></router-view>渲染整个工程的.vue组件。
+```
---
+++ src/views/login/Login.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="login-wrap" v-show="isclick">
+    <div class="login">
+      <div>
+        <div class="form-group">
+          <label>Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      isclick: true
+    };
+  }
+};
+</script>
+
+<style>
+.login-wrap {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  z-index: 99;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgb(0, 0, 0, 0.5);
+}
+.login {
+  opacity: 1;
+  width: 350px;
+  margin: 80px auto;
+  margin-top: 50px;
+  background: #fff;
+  padding: 10px 20px;
+  border-radius: 4px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+}
+</style>
