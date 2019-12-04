dfe6215df68b3495a7456e768ccdb3ccff848d5a
yuanchunyue <2075397144@qq.com>
2019-12-04 23:11:30 +0800

    修改了修改了修改了修改了修改了修改了修改了修改了修改了修改了修改了

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
     <Login></Login>
-=======
-    <!-- <Login></Login> -->
-
->>>>>>> dev
     <div id="nav">
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>|
--- src/main.js
+++ src/main.js
@@ -2,13 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-<<<<<<< HEAD
-import rest from "./assets/rest.css";
-=======
->>>>>>> dev
-
-Vue.config.productionTip = false;
-
 new Vue({
   rest,
   router,
