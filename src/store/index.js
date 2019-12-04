c493fe1e90281ae2f679183ac147e7c550b6239f
zry-web <1607153998@qq.com>
2019-12-04 22:45:25 +0800

    写了很多login组件

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,11 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import login from "./modules/login/login";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    login
   }
-})
+});
