93b18e84d9ff6c71464252a81d7c2728bac39094
yuanchunyue <2075397144@qq.com>
2019-12-09 09:47:10 +0800

    重新拉取

--- src/views/louplus/Course.vue
+++ src/views/louplus/Course.vue
@@ -1,6 +1,57 @@
-<template></template>
+<template>
+  <div class="full-page">
+    <div class="course-page">
+      <div class="course-content">
+        <div class="container">
+          <div class="course-header">
+            <h2>楼+课程</h2>
+          </div>
+          <div class="row">
+            <div class="col-md-4">
+              <div class="course-item">
+                <a href>
+                  <div class="items">
+                    <div class="item-header">
+                      <img
+                        class="item-img"
+                        src="https://dn-simplecloud.shiyanlou.com/course/1572342281698_楼+python基础课程.png"
+                        alt
+                      />
+                    </div>
+                    <div class="item-body">
+                      <p class="item-name">楼+之Python基础（随到随学班）</p>
+                      <p class="item-mark">——4周轻松掌握Python基础</p>
+                      <div class="item-content">
+                        <p class="content-name">22 个实验 + 11 个挑战</p>
+                        <p class="content-desc">涵盖知识模块：Python 基础语法，Python自动化，爬虫。</p>
+                      </div>
+                    </div>
+                    <div class="item-footer">
+                      <p class="new-prive">
+                        <span>限时优惠</span>
+                        <span>￥499</span>
+                      </p>
+                      <p class="old-price line-through">￥899</p>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 <script>
 export default {};
 </script>
 <style scoped>
-</style>>
+.full-page {
+  width: 100%;
+}
+.course-page {
+  padding: 70px 0 30px;
+  color: #fff;
+}
+</style>
